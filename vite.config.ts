65a9409312582bdd0db66f009ffbaf3d17f68e99
faiz1405 <yusufnurfaizip05@gmail.com>
2026-01-06 20:50:33 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   ssr: {
-    external: ["@prisma/client", "./generated/prisma"],
+    external: ["@prisma/client"],
     noExternal: ["@uiw/react-md-editor", "@uiw/react-markdown-preview"],
   },
 });
